0bf76c2744ffc20592a58d408a0fcab5a0c3be65
jayantrajput2018-ops <jayantrajput2018@gmail.com>
2026-03-02 15:16:49 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
+// 1. Find the "root" div in your HTML
 const rootElement = document.getElementById('root');
+
+// 2. Tell React to take control of that div
 const root = ReactDOM.createRoot(rootElement);
 
+// 3. Render your app inside it
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
