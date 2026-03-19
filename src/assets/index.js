f5ace2b4a2a7d6043c704222be790a7a7836e714
jayantrajput2018-ops <jayantrajput2018@gmail.com>
2026-03-19 06:32:43 +0000

    Resolved conflicts

---
+++ src/assets/index.js
@@ -0,0 +1,21 @@
+import eduskill from "./company/eduskill.png";
+import python from "./tech-stack/python.png";
+import termpw from "./projects/termpw.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import logo from "./logo.svg";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+export {
+  eduskill,
+  logo,
+  git,
+  github,
+  close,
+  menu,
+  html,
+  termpw,
+  python,
+};
